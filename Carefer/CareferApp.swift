fa3112a8030f65194e1769f4ba595e72a26f415a
Muhammad Kamal <mohamed.kamal@rpmanetworks.com>
2022-12-06 15:36:09 +0200

    Initial Commit

---
+++ Carefer/CareferApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareferApp.swift
+//  Carefer
+//
+//  Created by Muhammad Kamal on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CareferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
